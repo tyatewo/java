2c0e35758804dcb0253a74ef24a5a0945db1d002
Saori Sakai <124588744+tyatewo@users.noreply.github.com>
2023-07-25 14:36:40 +0000

    for,array

---
+++ review/array.java
@@ -0,0 +1,35 @@
+ // for文を用いて、「私の名前は◯◯です」と出力してください
+ class Main {
+   public static void main (String[], args) {
+     String[] names = {"にんじゃわんこ", "ひつじ仙人", "ベイビーわんこ"};
+   }
+
+   for(int i = 0; i < names.length; i++){
+     System.out.println("私の名前は"+names[i]+"です。");
+   }
+
+ }
+
+
+ // 配列にある奇数の和と偶数の和を求める
+
+class Main {
+  public static void main(String[] args) {
+    int[] numbers = {1, 4, 6, 9, 13, 16};
+
+
+    int oddSum = 0;
+    int evenSum = 0;
+
+    for(int number: numbers) {
+      if(number%2==0) {
+        evenSum+=number;
+      }else{
+        oddSum+=number;
+      }
+    }
+
+    System.out.println("奇数の和は"+oddSum+"です");
+    System.out.println("偶数の和は"+evenSum+"です");
+  }
+}
---
+++ review/for.java
@@ -0,0 +1,10 @@
+// for文
+// for文を用いて、10回ループする繰り返し処理を書いてみましょう。
+
+class main {
+  public static void main(String[] args) {
+    for(int i = 0; i <= 10; i++) {
+      System.out.println(i);
+    }
+  }
+}
