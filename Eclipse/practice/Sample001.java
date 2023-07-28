fc39a42f8e22cccc678f81a63db7ed462e542973
Saori Sakai <124588744+tyatewo@users.noreply.github.com>
2023-07-28 08:22:39 +0000

    practice

---
+++ Eclipse/practice/Sample001.java
@@ -0,0 +1,12 @@
+public class Sample001 {
+    public static void main(String[] args) {
+        number();
+    }
+
+    public static void number() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
+//test
