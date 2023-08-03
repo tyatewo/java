81dd44cd99f37ef9a41c892d934fb95d4911e262
Saori Sakai <124588744+tyatewo@users.noreply.github.com>
2023-08-03 10:20:51 +0000

    拡張for文

--- Eclipse/review/question3.java
+++ Eclipse/review/question3.java
@@ -16,3 +16,24 @@ class Main {
 
   }
 }
+
+
+
+
+class Main {
+  public static void main(String[] args) {
+
+    int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+
+    for(int number: numbers) {
+
+      if(number!=10) {
+        System.out.print(number+",");
+      }else{
+        System.out.print(number);
+      }
+
+    }
+
+  }
+}
